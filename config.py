0e01744ac16b76b4c6e7fc378ea7a8cfe9fe9f1f
melissa-koi <melissawangui3@gmail.com>
2021-04-25 12:33:24 +0300

    add main config.py file

---
+++ config.py
@@ -0,0 +1,56 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://koi:password@localhost/watchlist'
+    # UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # #Email Configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    # SIMPLEMDE_JS_IIFE = True
+    # SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://koi:password@localhost/watchlist_test'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://koi:password@localhost/watchlist')
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test': TestConfig
+}
