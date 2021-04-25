b575e3b150976419177d8a1a71246284a7fb4016
melissa-koi <melissawangui3@gmail.com>
2021-04-25 12:50:08 +0300

    modified the auth views

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+# from flask import render_template,request,redirect,url_for,abort
+# from . import main
+# from ..requests import get_movies,get_movie,search_movie
+# from .forms import ReviewForm,UpdateProfile
+# from ..models import Review,User,PhotoProfile
+# from flask_login import login_required,current_user
+# from .. import db,photos
+#
+#
+# @main.route('/')
+# def index():
+#     <p>
