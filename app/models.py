2ebc14d7e87ab3d7dc973b0661cc6bd20f5c7803
melissa-koi <melissawangui3@gmail.com>
2021-04-25 11:55:10 +0300

    add manage.py

--- app/models.py
+++ app/models.py
@@ -4,6 +4,38 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# class Review(db.Model):
+#
+#     __tablename__ = 'reviews'
+#     id = db.Column(db.Integer,primary_key = True)
+#     movie_id = db.Column(db.Integer)
+#     movie_title = db.Column(db.String)
+#     image_path = db.Column(db.String)
+#     movie_review = db.Column(db.String)
+#     posted = db.Column(db.Time,default=datetime.utcnow())
+#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+#
+#
+#
+#     def save_review(self):
+#         db.session.add(self)
+#         db.session.commit()
+#
+#
+#
+#     @classmethod
+#     def get_reviews(cls,id):
+#
+#         reviews = Review.query.filter_by(movie_id=id).all()
+#         return reviews
+#
+#
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
